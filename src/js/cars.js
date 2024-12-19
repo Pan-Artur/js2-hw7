8556ff1c55d0ffb36833ae3e9aa44bf2fa161425
Pan-Artur <pankovets.artur1@gmail.com>
2024-12-19 17:00:57 +0200

    working-project

---
+++ src/js/cars.js
@@ -0,0 +1,32 @@
+export const cars = [
+    {
+        photo: "./bmw.7cc953fa.webp",
+        mark: "BMW",
+        model: "M5 f90 competition",
+        price: "85 000$",
+    },
+    {
+        photo: "./mercedes-benz.f75818ad.webp",
+        mark: "Mercedes-Benz",
+        model: "560 sec amg 1989",
+        price: "21 000$",
+    },
+    {
+        photo: "./toyota.c98a158e.webp",
+        mark: "Toyota",
+        model: "Supra MK4",
+        price: "20 000$",
+    },
+    {
+        photo: "./nissan.ebf62774.webp",
+        mark: "Nissan",
+        model: "Skyline GT-R34",
+        price: "170 000$",
+    },
+    {
+        photo: "./mitsubishi.54ac4e5a.webp",
+        mark: "Mitsubishi",
+        model: "Lancer Evolution",
+        price: "7 600$",
+    },
+];
